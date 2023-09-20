a31c8ca9d02a3cb6451da6e61915a62c659762b1
Kevin harrington <mad.hephaestus@gmail.com>
2023-09-20 08:20:08 -0400

    adding cura

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo apt install -y git curl wget inkscape scratch
+sudo apt install -y git curl wget inkscape scratch docker.io  libfuse2
+
+curl -L https://github.com/Ultimaker/Cura/releases/download/5.4.0/UltiMaker-Cura-5.4.0-linux-modern.AppImage -o cura
+sudo mv cura /usr/local/bin/
+sudo chmod +x /usr/local/bin/cura
