ebc330b278784af48a21c8c0b27e64cf87be1039
hephaestus <hephaestus@hephaestus-iMac>
2023-09-20 09:23:39 -0400

    all as sudo

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-pkexec apt install -y git curl wget inkscape scratch docker.io  libfuse2 nodejs npm sssd-ad sssd-tools realmd adcli
+echo apt install -y git curl wget inkscape scratch docker.io  libfuse2 nodejs npm sssd-ad sssd-tools realmd adcli
 SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if (! test -e /usr/local/bin/cura) then
 	curl -L https://github.com/Ultimaker/Cura/releases/download/5.4.0/UltiMaker-Cura-5.4.0-linux-modern.AppImage -o $SCRIPT/cura
-	pkexec mv $SCRIPT/cura /usr/local/bin/
-	pkexec chmod +x /usr/local/bin/cura
+	echo mv $SCRIPT/cura /usr/local/bin/
+	echo chmod +x /usr/local/bin/cura
 fi
 ICON=$SCRIPT/Cura/resources/images/cura-icon.png
 if (! test -e $SCRIPT/Cura.desktop) then
 	if (! test -e $ICON) then
 		git clone https://github.com/Ultimaker/Cura.git $SCRIPT/Cura/
 	fi
-	pkexec cp $SCRIPT/Cura/resources/images/cura-icon.png /usr/local/bin/cura-icon.png
+	echo cp $SCRIPT/Cura/resources/images/cura-icon.png /usr/local/bin/cura-icon.png
 	echo "[Desktop Entry]
 	Version=1.0
 	Type=Application
@@ -25,7 +25,7 @@ if (! test -e $SCRIPT/Cura.desktop) then
 	Terminal=false
 	StartupNotify=false" > $SCRIPT/Cura.desktop
 	gio set $SCRIPT/Cura.desktop "metadata::trusted" yes
-	pkexec desktop-file-install $SCRIPT/Cura.desktop
+	echo desktop-file-install $SCRIPT/Cura.desktop
 fi
 SCRATCHJR=$SCRIPT/ScratchJr-Desktop/
 if (! test -e $SCRATCHJR) then
