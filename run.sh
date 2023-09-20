0c117e86bcb0d7133bcde8bcf5cdb394b7f43b97
hephaestus <hephaestus@hephaestus-iMac>
2023-09-20 09:13:56 -0400

    absolute location for cura

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@ pkexec apt install -y git curl wget inkscape scratch docker.io  libfuse2 nodejs 
 SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if (! test -e /usr/local/bin/cura) then
-	curl -L https://github.com/Ultimaker/Cura/releases/download/5.4.0/UltiMaker-Cura-5.4.0-linux-modern.AppImage -o cura
-	pkexec mv cura /usr/local/bin/
+	curl -L https://github.com/Ultimaker/Cura/releases/download/5.4.0/UltiMaker-Cura-5.4.0-linux-modern.AppImage -o $SCRIPT/cura
+	pkexec mv $SCRIPT/cura /usr/local/bin/
 	pkexec chmod +x /usr/local/bin/cura
 fi
 ICON=$SCRIPT/Cura/resources/images/cura-icon.png
 if (! test -e $SCRIPT/Cura.desktop) then
 	if (! test -e $ICON) then
-		git clone https://github.com/Ultimaker/Cura.git $SCRIPT/Cura
+		git clone https://github.com/Ultimaker/Cura.git $SCRIPT/Cura/
 	fi
 	pkexec cp $SCRIPT/Cura/resources/images/cura-icon.png /usr/local/bin/cura-icon.png
 	echo "[Desktop Entry]
