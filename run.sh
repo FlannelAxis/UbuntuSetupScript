d855af842c5c2bfb6c2dbcaaa0f6c166cdbfb1a1
Kevin Harrington <mad.hephaestus@gmail.com>
2023-10-10 16:15:56 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-set -e
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
