623dec811ad166213e1e46638d6b73de0bf0fecc
Kevin Harrington <mad.hephaestus@gmail.com>
2023-10-12 19:17:41 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
 sudo apt update
+sudo apt upgrade
 sudo apt purge -y bcmwl-kernel-source
 sudo apt install -y firmware-b43-installer
 
