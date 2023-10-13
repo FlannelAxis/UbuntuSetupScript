f922e92a79619c3006479d065f3483d8f73fff8c
Kevin Harrington <mad.hephaestus@gmail.com>
2023-10-13 10:01:17 -0400

    Create joinDomain.sh

---
+++ joinDomain.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Set the resolv.conf
+sudo chmod 777   /run/systemd/resolve/stub-resolv.conf
+echo "Diff of /run/systemd/resolve/stub-resolv.conf and the intended file:"
+sudo diff /run/systemd/resolve/stub-resolv.conf $SCRIPT/resolv.conf
+sudo cp $SCRIPT/resolv.conf  /run/systemd/resolve/stub-resolv.conf
+sudo chmod 755  /run/systemd/resolve/stub-resolv.conf
+
+#Join AD
+#Set the sssd to default to begin
+sudo cp /usr/lib/x86_64-linux-gnu/sssd/conf/sssd.conf /etc/sssd/
+sudo chmod 600 /etc/sssd/sssd.conf 
+sudo systemctl enable sssd
+sudo systemctl start sssd
+set -e
+echo "AD Domain Administrator password is here needed, please enter the domain passowrd:"
+sudo realm join -v bsch.bancroftschool.org
+echo "sudo pam-auth-update --enable mkhomedir"
+sudo pam-auth-update --enable mkhomedir
+
+# Use our configurations
+sudo chmod 777  /etc/sssd/sssd.conf
+sudo cp $SCRIPT/sssd.conf /etc/sssd/
+sudo chmod 600  /etc/sssd/sssd.conf
+echo "sudo systemctl enable sssd"
+# restart sssd to finish setup
+sudo systemctl enable sssd
+echo "sudo systemctl start sssd"
+sudo systemctl start sssd
+echo "sudo realm permit -a"
+sudo realm permit -a
+set +e
+echo "Diff of /etc/sssd/sssd.conf and the intended file:"
+sudo diff /etc/sssd/sssd.conf $SCRIPT/sssd.conf
+
+echo "Copying over SSH keys"
+cat $SCRIPT/id_rsa.pub > ~/.ssh/authorized_keys 
+cat $SCRIPT/id_ecdsa.pub  >> ~/.ssh/authorized_keys 
+
+#sudo mkdir -p /etc/skel/snap/firefox/common/
+#sudo cp -r $SCRIPT/.mozilla/ /etc/skel/snap/firefox/common/
+sudo rm -rf /etc/skel/snap/firefox/
+cd /home/
+for d in */ ; do
+    echo "Checking $d"
+    sudo rm -rf /home/$d/snap/firefox/
+done
