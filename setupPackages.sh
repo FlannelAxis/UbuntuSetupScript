c4ba4c926fb4fc40151f771de1ff76f71223242f
Kevin Harrington <mad.hephaestus@gmail.com>
2023-11-10 12:52:39 -0500

    Update setupPackages.sh

--- setupPackages.sh
+++ setupPackages.sh
@@ -18,7 +18,7 @@ else
 	echo "$GITHUBDESKTOP installed "
 fi
 
-sudo apt install -y git libncurses5 libpython2.7 mesa-utils openshot-qt python3-openshot ssh net-tools build-essential curl wget inkscape docker.io  libfuse2 nodejs npm sssd-ad sssd-tools realmd adcli krita obs-studio godot3 google-chrome-stable
+sudo apt install -y git pyserial libncurses5 libpython2.7 mesa-utils openshot-qt python3-openshot ssh net-tools build-essential curl wget inkscape docker.io  libfuse2 nodejs npm sssd-ad sssd-tools realmd adcli krita obs-studio godot3 google-chrome-stable
 sudo apt purge -y modemmanager scratch brltty
 
 SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
