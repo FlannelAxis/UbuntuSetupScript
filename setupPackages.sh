b5a43308822de93ee1090552a7d0c5d59f77bf90
Kevin Harrington <mad.hephaestus@gmail.com>
2023-12-18 10:14:06 -0500

    Update setupPackages.sh

--- setupPackages.sh
+++ setupPackages.sh
@@ -5,7 +5,7 @@ SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
 sudo add-apt-repository -y ppa:openshot.developers/ppa
-sudo add-apt-repository ppa:sunderme/texstudio
+sudo add-apt-repository -y ppa:sunderme/texstudio
 
 sudo apt update
 sudo apt  -y upgrade
