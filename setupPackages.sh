f7fb624762cce554f1850291288ac05c405a78ab
Kevin Harrington <kharrington@bancroftschool.org>
2024-04-22 13:16:21 -0400

    Whole script

--- setupPackages.sh
+++ setupPackages.sh
@@ -9,7 +9,7 @@ if (! test -e $SBA) then
 else
 	echo "$SBA installed "
 fi
-exit 0
+
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
 sudo add-apt-repository -y ppa:openshot.developers/ppa
