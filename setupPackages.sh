841112a752154f9d7288afd704ea76952e04127e
Kevin Harrington <kharrington@bancroftschool.org>
2024-05-15 08:14:31 -0400

    Add the -y

--- setupPackages.sh
+++ setupPackages.sh
@@ -14,7 +14,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
 sudo add-apt-repository -y ppa:openshot.developers/ppa
 sudo add-apt-repository -y ppa:sunderme/texstudio
-sudo add-apt-repository ppa:sylvain-pineau/kazam
+sudo add-apt-repository -y ppa:sylvain-pineau/kazam
 
 sudo apt update
 sudo apt  -y upgrade
